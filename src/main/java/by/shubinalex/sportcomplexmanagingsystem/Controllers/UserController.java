71c141e206852e36b0f79696865375358fad2b81
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-04-17 02:09:40 +0300

    First backend autocommit

--- src/main/java/by/shubinalex/sportcomplexmanagingsystem/Controllers/UserController.java
+++ src/main/java/by/shubinalex/sportcomplexmanagingsystem/Controllers/UserController.java
@@ -1,15 +1,26 @@
 package by.shubinalex.sportcomplexmanagingsystem.Controllers;
+import by.shubinalex.sportcomplexmanagingsystem.entities.ComplexFacility;
+import by.shubinalex.sportcomplexmanagingsystem.entities.Training;
 import by.shubinalex.sportcomplexmanagingsystem.entities.User;
 import by.shubinalex.sportcomplexmanagingsystem.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepo userRepo;
+
+    @RequestMapping(value = "/api/account_managing", method = RequestMethod.POST)
+    public void updateUserStatus(@RequestBody User user) {
+        System.out.println(user);
+        Optional<User> updated_status_user = userRepo.findByUserLogin(user.getUserLogin());
+        updated_status_user.get().setStatus(user.getStatus());
+        userRepo.save(updated_status_user.get());
+
+    }
+
 
 }
